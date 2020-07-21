d11c518a6df1a05ff7b87c7af12a6478436737c8
Colman Currie <curriecolman@gmail.com>
2020-07-20 17:11:19 -0700

    'add files'

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const app = express();
 
 const port = 3000;
 
-app.get('/', (request, response) = {
-    response.send('Hello Express :)'),
+app.get('/', (request, response) => {
+    response.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.get('/speakers', (request, response) => {
+    response.sendFile(path.join(__dirname, './static/speakers.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Express server listening on port ${port}!`);
 });
